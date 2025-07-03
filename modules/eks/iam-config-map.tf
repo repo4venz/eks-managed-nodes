3c35a16189d74181fdf2e707491798c429e8c665
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-03 02:07:30 +0100

    updated aws auth config

--- modules/eks/iam-config-map.tf
+++ modules/eks/iam-config-map.tf
@@ -32,7 +32,7 @@ resource "null_resource" "eks_delete_configmap_exec" {
 	    null_resource.eks_get_config_exec
 	  ]
 	}
-
+*/
 
 # Define the aws-auth configmap
 resource "kubernetes_config_map" "aws_auth" {
@@ -65,10 +65,7 @@ resource "kubernetes_config_map" "aws_auth" {
   }
 
   	  depends_on = [
-	      aws_eks_cluster.demo_eks_cluster,
-        null_resource.eks_get_config_exec,
-        data.http.eks_cluster_readiness[0]
+        null_resource.eks_get_config_exec
 	  ]
 }
 
-*/
