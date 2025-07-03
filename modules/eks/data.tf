51ac7e710e5bbbc5008f69a5179d6de4bd595a34
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-03 06:37:19 +0100

    updated workflow

--- modules/eks/data.tf
+++ modules/eks/data.tf
@@ -4,15 +4,6 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
- /**
-data "http" "eks_cluster_readiness" {
-
-  url            = join("/", [aws_eks_cluster.demo_eks_cluster.endpoint, "healthz"]) #refering output variable values
-  ca_certificate = base64decode(aws_eks_cluster.demo_eks_cluster.certificate_authority[0].data) #refering output variable values
-  timeout        = var.eks_readiness_timeout
-}
-
- */
 
 data "aws_iam_session_context" "current" {
   arn = data.aws_caller_identity.current.arn
