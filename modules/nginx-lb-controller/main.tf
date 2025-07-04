8fc8c46934ea079910d6fd3ec66bb9a7b88c51a1
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-04 12:21:10 +0100

    updated helm

--- modules/nginx-lb-controller/main.tf
+++ modules/nginx-lb-controller/main.tf
@@ -1,4 +1,15 @@
  
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.eks_cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
+    cluster_ca_certificate = module.eks.eks_cluster_certificate_authority_data
+    #config_path = "./.kube/config"
+    config_path ="/home/runner/.kube/config"
+  }
+}
+
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
