14c3a537d3aa34a3817a39f732b1fff14a732d48
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-05 20:41:54 +0100

    added csi driver

--- modules/eks/data.tf
+++ modules/eks/data.tf
@@ -9,4 +9,9 @@ data "aws_iam_session_context" "current" {
   arn = data.aws_caller_identity.current.arn
 }
 
+
+data "aws_eks_addon_version" "ebs_csi" {
+  addon_name   = "aws-ebs-csi-driver"
+  kubernetes_version = aws_eks_cluster.demo_eks_cluster.version
+}
  
