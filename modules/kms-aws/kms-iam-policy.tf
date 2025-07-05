684cfd29b94ae43659b04aea4f4cc1421c133d93
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-05 15:51:20 +0100

    updated kms permission

--- modules/kms-aws/kms-iam-policy.tf
+++ modules/kms-aws/kms-iam-policy.tf
@@ -1,60 +1,82 @@
-//kms key policy
 
-	data "aws_iam_policy_document" "kms_key_policy" {
-	  statement {
-	    sid = "EnableAdminAccess"
-	
-	    principals {
-	      type        = "AWS"
-	      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
-	    }
-	
-	    actions = [
-	      "kms:Create*",
-	      "kms:Describe*",
-	      "kms:Enable*",
-	      "kms:List*",
-	      "kms:Put*",
-	      "kms:Update*",
-	      "kms:Revoke*",
-	      "kms:Disable*",
-	      "kms:Get*",
-	      "kms:Delete*",
-	      "kms:ScheduleKeyDeletion",
-	      "kms:CancelKeyDeletion",
-	      "kms:TagResource",
-	      "kms:UntagResource",
-          "kms:ListAliases"
-	    ]
-	
-	    resources = ["*"]
-	  }
-	  statement {
-	    sid = "EnableAccessForCloudWatchLogs"
-	
-	    principals {
-	      type = "Service"
-	      identifiers = [
-	        "cloudwatch.amazonaws.com",
-	        "eks.amazonaws.com",
-	        "eks-fargate-pods.amazonaws.com",
-	        "logs.amazonaws.com"
-	      ]
-	    }
-	
-	    actions = [
-	      "kms:Encrypt",
-	      "kms:Decrypt",
-	      "kms:ReEncrypt",
-	      "kms:GenerateDataKey",
-	      "kms:DescribeKey",
-	      "kms:ScheduleKeyDeletion",
-	      "kms:CancelKeyDeletion",
-	      "kms:TagResource",
-	      "kms:UntagResource",
-          "kms:ListAliases"
-	    ]
-	
-	    resources = ["*"]
-	  }
-	}
+
+data "aws_iam_policy_document" "kms_key_policy" {
+  statement {
+    sid     = "AllowEKS"
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "AllowEC2EBS"
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey"
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+
+      values = ["ec2.${data.aws_region.current.id}.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid     = "AllowCloudWatchLogs"
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.id}.amazonaws.com"]
+    }
+
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "AllowRootAccount"
+    effect  = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+}
