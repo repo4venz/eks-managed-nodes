eeeb88318d1f229dce8caf23ebf923455a149002
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-06 18:07:56 +0100

    fixed oidc

--- modules/fluentbit/data.tf
+++ modules/fluentbit/data.tf
@@ -14,7 +14,7 @@ data "aws_eks_cluster_auth" "this" {
 
 
 data "aws_iam_openid_connect_provider" "oidc" {
-  url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
 
 
