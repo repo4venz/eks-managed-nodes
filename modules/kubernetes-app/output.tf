ed4c8f4d97b9b663d6575f4d57d23404fe448b93
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-06 12:29:58 +0100

    fixed lb name

--- modules/kubernetes-app/output.tf
+++ modules/kubernetes-app/output.tf
@@ -7,13 +7,13 @@ data "kubernetes_ingress" "example" {
 
 # Display load balancer hostname (typically present in AWS)
 output "load_balancer_hostname" {
-   value = data.kubernetes_ingress.example.status.0.load_balancer.0.ingress.0.hostname
+   value = data.kubernetes_ingress.game-app-ingress.status.0.load_balancer.0.ingress.0.hostname
 }
 
 
 # Display load balancer IP (typically present in GCP, or using Nginx ingress controller)
 output "load_balancer_ip" {
-  value  = data.kubernetes_ingress.example.status.0.load_balancer.0.ingress.0.ip
+  value  = data.kubernetes_ingress.game-app-ingress.status.0.load_balancer.0.ingress.0.ip
 }
 
 
