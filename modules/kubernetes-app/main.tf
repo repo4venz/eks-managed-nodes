52be00fe94c6cef589a18b8fb796d017f3569ccf
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-06 14:14:39 +0100

    updated ingress

--- modules/kubernetes-app/main.tf
+++ modules/kubernetes-app/main.tf
@@ -1,17 +1,4 @@
 
-/*
-resource "kubernetes_namespace" "application_namespace" {
-  count = var.create_namespace ? 1 : 0
-  metadata {
-    labels = {
-      "app.kubernetes.io/name" = "sample-game-app"
-    }
-    name = var.app_namespace
-  }
-}
-*/
-
- 
 
 resource "null_resource" "create_namespace_if_not_exists" {
   provisioner "local-exec" {
@@ -30,13 +17,13 @@ resource "kubernetes_deployment" "game-app" {
     name      = "game-app"
     namespace =  var.app_namespace #kubernetes_namespace.application_namespace.metadata.0.name
     labels    = {
-      "app.kubernetes.io/name" = "sample-game-app"
+      "app.kubernetes.io/name" = "game-app"
     }
   }
   spec {
     selector {
       match_labels = {
-        "app.kubernetes.io/name" = "sample-game-app"
+        "app.kubernetes.io/name" = "game-app"
       }
     }
     replicas = 3
@@ -44,14 +31,14 @@ resource "kubernetes_deployment" "game-app" {
     template {
       metadata {
         labels = {
-          "app.kubernetes.io/name" = "sample-game-app"
+          "app.kubernetes.io/name" = "game-app"
         }
       }
 
       spec {
         container {
           image = "public.ecr.aws/l6m2t8p7/docker-2048:latest"
-          name  = "sample-game-app"
+          name  = "game-app"
           image_pull_policy = "Always"
           port {
             container_port = 80
@@ -70,7 +57,7 @@ resource "kubernetes_service_v1" "game-app-service" {
   }
   spec {
     selector = {
-        "app.kubernetes.io/name" = "sample-game-app"
+        "app.kubernetes.io/name" = "game-app"
     }
     port {
       port        = 80
@@ -98,11 +85,9 @@ resource "kubernetes_ingress_v1" "game-app-ingress" {
   }
   spec {
     rule {
-      host = "game-app.suvendu.public-dns.aws"
       http {
         path {
-          path      = "/"
-          path_type = "Prefix"
+          path = "/*"
           backend {
             service {
               name = kubernetes_service_v1.game-app-service.metadata.0.name
@@ -115,10 +100,8 @@ resource "kubernetes_ingress_v1" "game-app-ingress" {
       }
     }
   }
-
- depends_on = [
+  depends_on = [
     kubernetes_service_v1.game-app-service
   ]
 }
-
 
