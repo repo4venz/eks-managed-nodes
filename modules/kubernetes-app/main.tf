59a8ddb6578d2f85c81c8daa41184cd49ecd5962
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-07 22:22:56 +0100

    updated application

--- modules/kubernetes-app/main.tf
+++ modules/kubernetes-app/main.tf
@@ -101,6 +101,7 @@ resource "kubernetes_ingress_v1" "this" {
     namespace = var.app_namespace
     annotations = {
       "kubernetes.io/ingress.class" : "nginx"
+      "external-dns.alpha.kubernetes.io/hostname" = "game-app.suvendu.public-dns.aws"
     }
   }
 
