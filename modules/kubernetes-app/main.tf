c942e2c43dc08246c46bc42450d0a0c5c4d9bc8d
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-07 23:18:42 +0100

    updated external dns annotation

--- modules/kubernetes-app/main.tf
+++ modules/kubernetes-app/main.tf
@@ -13,19 +13,7 @@ resource "null_resource" "create_namespace_if_not_exists" {
 
 # Terraform module to deploy docker-2048 to EKS using NGINX Ingress
 
-/*
-# -------------------
-# NAMESPACE
-# -------------------
-resource "kubernetes_namespace" "this" {
-  metadata {
-    name = var.app_namespace
-  }
-   depends_on = [null_resource.create_namespace_if_not_exists]
-}
-*/
-
-
+ 
 # -------------------
 # DEPLOYMENT
 # -------------------
@@ -102,6 +90,7 @@ resource "kubernetes_ingress_v1" "this" {
     namespace = var.app_namespace
     annotations = {
       "kubernetes.io/ingress.class" : "nginx"
+      "external-dns.alpha.kubernetes.io/hostname" = "sample-app.suvendu.public-dns.aws"
     }
   }
 
@@ -128,101 +117,4 @@ resource "kubernetes_ingress_v1" "this" {
 }
 
 
-
-
-/**  OLD CODE
-
-resource "kubernetes_deployment" "game-app" {
-  metadata {
-    name      = "game-app"
-    namespace =  var.app_namespace #kubernetes_namespace.application_namespace.metadata.0.name
-    labels    = {
-      "app.kubernetes.io/name" = "game-app"
-    }
-  }
-  spec {
-    selector {
-      match_labels = {
-        "app.kubernetes.io/name" = "game-app"
-      }
-    }
-    replicas = 3
-
-    template {
-      metadata {
-        labels = {
-          "app.kubernetes.io/name" = "game-app"
-        }
-      }
-
-      spec {
-        container {
-          image = "public.ecr.aws/l6m2t8p7/docker-2048:latest"
-          name  = "game-app"
-          image_pull_policy = "Always"
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
-   depends_on = [null_resource.create_namespace_if_not_exists]
-}
-
-resource "kubernetes_service_v1" "game-app-service" {
-  metadata {
-    name      = "game-app-service"
-    namespace = var.app_namespace # kubernetes_namespace.application_namespace.metadata.0.name
-  }
-  spec {
-    selector = {
-        "app.kubernetes.io/name" = "game-app"
-    }
-    port {
-      port        = 80
-      target_port = 80
-      protocol    = "TCP"
-    }
-    type = "NodePort"
-  }
-  depends_on = [kubernetes_deployment.game-app]
-}
-
-
-
-resource "kubernetes_ingress_v1" "game-app-ingress" {
-  wait_for_load_balancer = true
-  metadata {
-    name = "game-app-ingress"
-    namespace = var.app_namespace #kubernetes_namespace.application_namespace.metadata.0.name
-    annotations = {
-      "kubernetes.io/ingress.class"           = "nginx"  #"alb"
-      "nginx.ingress.kubernetes.io/rewrite-target"      = "/"
-      "nginx.ingress.kubernetes.io/ssl-redirect"        = "false"
-      "external-dns.alpha.kubernetes.io/hostname" = "game-app.suvendu.public-dns.aws"
-    }
-  }
-  spec {
-    rule {
-      http {
-        path {
-          path = "/*"
-          backend {
-            service {
-              name = kubernetes_service_v1.game-app-service.metadata.0.name
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  depends_on = [
-    kubernetes_service_v1.game-app-service
-  ]
-}
-
-*/
+ 
