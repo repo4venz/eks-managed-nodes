e80c91ab0d358c8495867228c3c1c4dcfe72fb85
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-09 15:04:43 +0100

    updated EBS CSI role

--- modules/eks/ebs-csi-driver-addon.tf
+++ modules/eks/ebs-csi-driver-addon.tf
@@ -32,12 +32,12 @@ resource "aws_iam_policy_attachment" "ebs_csi_iam_policy" {
   count = var.include_ebs_csi_driver_addon ? 1 : 0
 
   name       = substr("${aws_eks_cluster.demo_eks_cluster.name}-ebs-csi-driver-policy",0,64)  
-  roles      = aws_iam_role.eks_worker_nodes_role.name   # adjust if needed
+  roles      = aws_iam_role.ebs_csi_driver_role[0].name   # adjust if needed
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
-
-
  
+
+   
 
 # 2. Enable the EBS CSI Driver as an EKS add-on
 resource "aws_eks_addon" "ebs_csi" {
