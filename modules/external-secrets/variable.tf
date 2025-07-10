e626e7e13e8392b70a87d8f8c0aa7d28e6c9daf7
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-10 22:34:27 +0100

    fixed var issue

--- modules/external-secrets/variable.tf
+++ modules/external-secrets/variable.tf
@@ -26,12 +26,13 @@ variable "external_secret_chart_version" {
 
 variable "aws_test_secrets" {
   type = list(object({
-    description = "List of Secrets of AWS Secrets Manager and Kubernetes Application Namespace. It will map the which secrets will be accessed from which namespace"
-    secret_name           = string
+     secret_name           = string
     application_namespace = string
     k8s_secret_store_name = string
   }))
   default = []
+  description = "List of Secrets of AWS Secrets Manager and Kubernetes Application Namespace. It will map the which secrets will be accessed from which namespace"
+   
 }
 
  
