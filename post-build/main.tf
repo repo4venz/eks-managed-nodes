c106a3545fcaff847dba6ac5c2fdac864fb51ceb
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-11 19:47:09 +0100

    updated post build

--- post-build/main.tf
+++ post-build/main.tf
@@ -1,7 +1,7 @@
 
 module "lets-encrypt" {
   count = var.include_lets_encrypt_ca_module ? 1 : 0
-  source             = "./modules/lets-encrypt"
+  source             = "../modules/lets-encrypt"
   environment        =  var.environment
   acme_environment   = "prod"    # Let's Encrypt ACME env = prod is required for valid ssl certs in browser                         
   
@@ -11,7 +11,7 @@ module "lets-encrypt" {
 
 module "kube-cost" {
   count = var.include_kubecost_module ? 1 : 0
-  source                    = "./modules/kube-cost"
+  source                    = "../modules/kube-cost"
   k8s_cluster_name          =  "${var.cluster_name}-${var.environment}" #module.eks.eks_cluster_name
   kubecost_chart_version    =  var.kubecost_chart_version
   environment               =  var.environment
@@ -23,7 +23,7 @@ module "kube-cost" {
 
 module "external-secrets" {
   count = var.include_external_secrets_module ? 1 : 0
-  source                        = "./modules/external-secrets"
+  source                        = "../modules/external-secrets"
   k8s_cluster_name              =  "${var.cluster_name}-${var.environment}" #module.eks.eks_cluster_name
   external_secret_chart_version =  var.external_secret_chart_version
   aws_test_secrets              =  var.aws_test_secrets  ## This is only testing purpose
