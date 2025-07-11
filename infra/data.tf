441536ae7853b7dd63e2913b185cb58fc65876e6
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-11 18:23:10 +0100

    added data

--- infra/data.tf
+++ infra/data.tf
@@ -7,3 +7,7 @@ data "aws_region" "current" {}
  data "aws_eks_cluster_auth" "aws_iam_authenticator" {
   name = module.eks.eks_cluster_name
 }
+
+data "aws_eks_cluster" "this" {
+  name =  "${var.cluster_name}-${var.environment}" # Replace with your cluster name
+}
