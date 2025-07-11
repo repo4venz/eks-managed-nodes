bacf6fbf8af53bbdace92d811921de07de0bb45c
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-11 11:51:24 +0100

    rearranged modules

---
+++ post-build/data.tf
@@ -0,0 +1,9 @@
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+ data "aws_eks_cluster_auth" "aws_iam_authenticator" {
+  name = module.eks.eks_cluster_name
+}
