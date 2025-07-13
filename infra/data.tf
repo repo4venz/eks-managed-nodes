2ad9f440ffc76e3c8760e7c4abc37bd6d5fe79fb
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-13 08:37:13 +0100

    updated data and local

--- infra/data.tf
+++ infra/data.tf
@@ -7,7 +7,8 @@ data "aws_region" "current" {}
  data "aws_eks_cluster_auth" "aws_iam_authenticator" {
   name = module.eks.eks_cluster_name
 }
-
+/*
 data "aws_eks_cluster" "this" {
   name =  module.eks.eks_cluster_name  #"${var.cluster_name}-${var.environment}" # Replace with your cluster name
 }
+*/
--- infra/local.tf
+++ infra/local.tf
@@ -4,9 +4,12 @@ locals {
     aws_eks_cluster_endpoint       = data.aws_eks_cluster.this.endpoint
     aws_partition_id               = data.aws_partition.current.partition
     aws_region_name                = data.aws_region.current.id
-    eks_cluster_id                 = data.aws_eks_cluster.this.name
-    eks_oidc_issuer_url            = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
-    eks_oidc_provider_arn          = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${data.aws_eks_cluster.this.identity[0].oidc[0].issuer}"
-    eks_certificate_authority_data = try(base64decode(data.aws_eks_cluster.this.certificate_authority[0].data), "")
+    eks_cluster_id                 = module.eks.eks_cluster_name
+    eks_oidc_issuer_url            = module.eks.oidc_provider #module.eks.eks_cluster_name.identity[0].oidc[0].issuer
+    eks_oidc_provider_arn          = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${module.eks.oidc_provider}"
+    eks_certificate_authority_data = module.eks.eks_cluster_certificate_authority_data #try(base64decode(module.eks.eks_cluster_name.certificate_authority[0].data), "")
     tags                           = var.tags
 }
+
+
+ 
