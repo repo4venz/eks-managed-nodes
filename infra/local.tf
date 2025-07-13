cd5bc7939d96ac9c173c77d12568b0c1fea0da9c
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-13 08:39:35 +0100

    updated local

--- infra/local.tf
+++ infra/local.tf
@@ -1,7 +1,7 @@
 locals {
     aws_caller_identity_account_id = data.aws_caller_identity.current.account_id
     aws_caller_identity_arn        = data.aws_caller_identity.current.arn
-    aws_eks_cluster_endpoint       = data.aws_eks_cluster.this.endpoint
+    aws_eks_cluster_endpoint       = module.eks.eks_cluster_endpoint
     aws_partition_id               = data.aws_partition.current.partition
     aws_region_name                = data.aws_region.current.id
     eks_cluster_id                 = module.eks.eks_cluster_name
