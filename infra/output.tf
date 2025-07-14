54dce9e924f219556c8f82972fc76b8477f78a1b
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-14 21:26:47 +0100

    remvooe all the nodes

--- infra/output.tf
+++ infra/output.tf
@@ -3,3 +3,9 @@ output "debug_spot_node_groups_customised_config" {
 }
 
  
+output "debug_ondemand_node_groups_customised_config" {
+  value = local.ondemand_node_groups_customised_config
+}
+
+ 
+ 
