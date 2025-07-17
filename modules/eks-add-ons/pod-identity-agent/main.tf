a1e74159d9eb8245e8e9388e0cef01cf8f6b8e9e
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-17 18:03:32 +0100

    updated addon name

--- modules/eks-add-ons/pod-identity-agent/main.tf
+++ modules/eks-add-ons/pod-identity-agent/main.tf
@@ -2,8 +2,10 @@
 
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name                = data.aws_eks_cluster.this.name
-  addon_name                  = data.aws_eks_addon_version.pod_identity_agent.addon_name #"aws-eks-pod-identity-agent"
-  #addon_version               = data.aws_eks_addon_version.pod_identity_agent.kubernetes_version # optional (e.g. "v1.16.0-eksbuild.1")
+  addon_name                  = data.aws_eks_addon_version.pod_identity_agent.addon_name   #"eks-pod-identity-agent"
+  addon_version               = data.aws_eks_addon_version.pod_identity_agent.version # optional (e.g. "v1.16.0-eksbuild.1")
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
+
+#aws eks create-addon --cluster-name $EKS_CLUSTER_NAME --addon-name eks-pod-identity-agent
