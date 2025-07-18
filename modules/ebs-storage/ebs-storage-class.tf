4d9c2dca318ddd9f68743aeb288c0d18903dd1b0
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-18 17:49:16 +0100

    fixed default

--- modules/ebs-storage/ebs-storage-class.tf
+++ modules/ebs-storage/ebs-storage-class.tf
@@ -20,9 +20,6 @@ resource "kubernetes_storage_class_v1" "ebs_sc" {
 resource "kubernetes_storage_class_v1" "ebs_sc_retain" {
   metadata {
     name = "ebs-gp3-sc-retain"
-    annotations = {
-      "storageclass.kubernetes.io/is-default-class" = "true"
-    }
   }
   storage_provisioner = "ebs.csi.aws.com"
   volume_binding_mode = "WaitForFirstConsumer"
