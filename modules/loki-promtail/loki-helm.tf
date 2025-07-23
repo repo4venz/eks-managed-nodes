bc2f3cb59eb582b9ebfbeaaced24bf693ceea095
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-23 23:22:00 +0100

    fixed helm

--- modules/loki-promtail/loki-helm.tf
+++ modules/loki-promtail/loki-helm.tf
@@ -434,9 +434,7 @@ values = [
             s3forcepathstyle = true
           }
           # Moved shared_store to index_shipper
-          index_shipper = {
-            shared_store = "aws"
-          }
+          shared_store = "aws"
           tsdb_shipper = {
             active_index_directory = "/var/loki/index"
             cache_location         = "/var/loki/cache"
