efc985ac8921ae16d046b7e2fb95ba4700678d8d
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-23 15:13:33 +0100

    updated helm chart

--- modules/agentic-ai/k8sGPT-operator/k8sgpt-helm.tf
+++ modules/agentic-ai/k8sGPT-operator/k8sgpt-helm.tf
@@ -24,7 +24,7 @@ resource "helm_release" "k8sgpt" {
         namespace = var.prometheus_namespace  # Where Prometheus is installed
         interval  = "30s"
         additionalLabels = {
-            release = "kube-prometheus"
+            release = "kube-prometheus-stack"  # Adjust this label based on your Prometheus setup
         }
       }
     resources = {
@@ -40,6 +40,10 @@ resource "helm_release" "k8sgpt" {
       prometheus = {
         enabled = true
         namespace = var.prometheus_namespace  # Where Prometheus is installed
+      }
+      prometheusOperator = {
+        enabled = true
+        namespace = var.prometheus_namespace  # Where Prometheus Operator is installed
       }
       grafana = {
         enabled = true
