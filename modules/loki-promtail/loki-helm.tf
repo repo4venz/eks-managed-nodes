610a5782ebeb9d7e1bc6107ce46b02ad0b691cde
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-23 21:11:56 +0100

    updated helm

--- modules/loki-promtail/loki-helm.tf
+++ modules/loki-promtail/loki-helm.tf
@@ -235,7 +235,7 @@ resource "helm_release" "loki" {
 }
 */
 
-
+/*
 resource "helm_release" "loki" {
     name             = "loki"
     repository       = "https://grafana.github.io/helm-charts"
@@ -357,4 +357,33 @@ resource "helm_release" "loki" {
         aws_iam_role_policy_attachment.loki_policy_attachment,
         aws_s3_bucket.loki_storage
     ]
+}
+*/
+
+resource "helm_release" "loki" {
+  name             = "loki"
+  repository       = "https://grafana.github.io/helm-charts"
+  chart            = "loki"
+  version          = var.loki_chart_version
+  namespace        = var.k8s_namespace
+  create_namespace = true
+  atomic           = false  # Set to false to debug installation issues
+  cleanup_on_fail  = true
+  timeout          = 300
+
+  # Use a values file instead of inline values
+  values = [
+    templatefile("${path.module}/loki-values.yaml", {
+      loki_service_account_name = var.loki_service_account_name
+      loki_role_arn = aws_iam_role.loki_role.arn
+      s3_bucket_name = aws_s3_bucket.loki_storage.id
+      aws_region = data.aws_region.current.name
+      ebs_storage_class_name = var.ebs_storage_class_name
+    })
+  ]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.loki_policy_attachment,
+    aws_s3_bucket.loki_storage
+  ]
 }
