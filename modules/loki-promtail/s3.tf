6512dded6b26b096da5a71db7bc1b023daf0f333
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-24 20:16:07 +0100

    updated s3

--- modules/loki-promtail/s3.tf
+++ modules/loki-promtail/s3.tf
@@ -37,25 +37,24 @@ resource "aws_s3_bucket_public_access_block" "loki_block_public" {
 resource "null_resource" "s3_force_delete" {
   triggers = {
     bucket_name = aws_s3_bucket.loki_storage.name  # Trigger re-creation if bucket name changes
+    region_name = data.aws_region.current.id
   }
 
   provisioner "local-exec" {
     when    = destroy
     command = <<EOT
       # Check if bucket exists first
-      if aws s3 ls "s3://${self.triggers.bucket_name}" --no-sign-request 2>&1 | grep -q 'NoSuchBucket'; then
+      if aws s3 ls "s3://${self.triggers.bucket_name}" --no-sign-request --region ${self.triggers.region_name} 2>&1 | grep -q 'NoSuchBucket'; then
         echo "Bucket ${self.triggers.bucket_name} does not exist"
         exit 0
       fi
 
       # Empty and delete bucket
-      aws s3 rm "s3://${self.triggers.bucket_name}" --recursive && \
-      aws s3 rb "s3://${self.triggers.bucket_name}" --force
+      aws s3 rm "s3://${self.triggers.bucket_name}" --recursive --region ${self.triggers.region_name} && \
+      aws s3 rb "s3://${self.triggers.bucket_name}" --force --region ${self.triggers.region_name}
     EOT
 
-    environment = {
-      AWS_DEFAULT_REGION = aws_region.current.id
-    }
+  
   }
   depends_on = [ aws_s3_bucket.loki_storage ]
 }
