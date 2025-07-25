d933baf4b16efea38b299bd7f75c6e8551445785
Suvendu Mandal <suvendu.mandal@gmail.com>
2025-07-25 10:55:19 +0100

    updated version

--- ai-build/version.tf
+++ ai-build/version.tf
@@ -24,7 +24,7 @@ terraform {
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.19.0"
+      version = ">= 1.14.0" # Specify your desired version
     }
     grafana = {
       source  = "grafana/grafana"
